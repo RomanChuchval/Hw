1af25e4de0a246928675a80b2fe78bad11aa7971
Roman Chuchval <rchuchval@gmail.com>
2023-02-17 16:59:18 +0300

    homework #7

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -52,7 +52,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                     type={'radio'}
                     // name, checked, value делают студенты
                     name={name}
-                    value={o.value}
+                    value={o.id}
                     checked={o.id.toString() === value}
                     onChange={onChangeCallback}
                     {...restProps}
